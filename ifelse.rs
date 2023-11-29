5407882c156506e9a04a0e70c95d76596e9768d2
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-29 20:54:47 +0530

    if else in rust added

---
+++ ifelse.rs
@@ -0,0 +1,23 @@
+fn main() {
+   
+    let num = 10;
+    
+
+    if num < 0 {
+    
+        println!("{} is negative", num);
+    
+    } 
+    
+    else if num > 0 {
+      
+        println!("{} is positive", num);
+    } 
+    
+    else {
+       
+        println!("{} is zero", num);
+    }
+
+ 
+}
