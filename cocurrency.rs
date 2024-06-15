5ff255b2f361b2679b6a2c40eaec5fe81b58f08d
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2024-06-15 22:40:30 +0530

    Create cocurrency.rs

---
+++ cocurrency.rs
@@ -0,0 +1,19 @@
+use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+
+fn concurrency() {
+    let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..5 {
+        let (data, tx) = (Arc::clone(&data), tx.clone());
+        thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            data.push(4); // Modify the shared data
+            tx.send(()).unwrap();
+        });
+    }
+    for _ in 0..5 {
+        rx.recv().unwrap();
+    }
+    println!("Final Data: {:?}", data);
+}
